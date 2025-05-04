4915bcd771d37caa1dd8f00c8a4fbecad626c79f
vidfefe <malinovskaaviktoria7@gmail.com>
2025-05-04 11:09:24 +0300

    feat: add commands in index.js

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ import {
 import path from "path";
 import { copyFile } from "fs";
 import { calculateHash } from "./hash.js";
+import { compressFile, decompressFile } from "./compression.js";
+import {
+  logArchitecture,
+  logCPUs,
+  logEOL,
+  logHomedir,
+  logUsername,
+} from "./os.js";
 
 const main = async () => {
   const username = process.argv.find((arg) => arg.startsWith("--username="));
@@ -49,25 +57,33 @@ const main = async () => {
           const newPath = resolvePath(currentDir, args[0]);
           const changedDir = changeDirectory(newPath);
           if (changedDir) currentDir = changedDir;
+        } else {
+          console.log("Operation failed");
         }
         break;
       case "ls":
-        const files = listFiles(currentDir);
+        listFiles(currentDir);
         break;
       case "cat":
         if (args.length > 0) {
           const filePath = resolvePath(currentDir, args[0]);
           readFile(filePath);
+        } else {
+          console.log("Operation failed");
         }
         break;
       case "add":
         if (args.length > 0) {
           createFile(args[0], currentDir);
+        } else {
+          console.log("Operation failed");
         }
         break;
       case "mkdir":
         if (args.length > 0) {
           createDir(args[0], currentDir);
+        } else {
+          console.log("Operation failed");
         }
         break;
       case "rn":
@@ -75,6 +91,8 @@ const main = async () => {
           const oldPath = path.join(currentDir, args[0]);
           const newName = args[1];
           renameFile(oldPath, newName);
+        } else {
+          console.log("Operation failed");
         }
         break;
       case "cp":
@@ -82,6 +100,8 @@ const main = async () => {
           const oldPath = path.join(currentDir, args[0]);
           const newName = path.join(currentDir, args[1]);
           copyFile(oldPath, newName);
+        } else {
+          console.log("Operation failed");
         }
         break;
       case "mv":
@@ -89,18 +109,64 @@ const main = async () => {
           const oldPath = path.join(currentDir, args[0]);
           const newName = path.join(currentDir, args[1]);
           moveFile(oldPath, newName);
+        } else {
+          console.log("Operation failed");
         }
         break;
       case "rm":
         if (args.length > 0) {
           const filePath = path.join(currentDir, args[0]);
           deleteFile(filePath);
+        } else {
+          console.log("Operation failed");
         }
         break;
       case "hash":
         if (args.length > 0) {
           const filePath = resolvePath(currentDir, args[0]);
           calculateHash(filePath);
+        } else {
+          console.log("Operation failed");
+        }
+
+        break;
+      case "compress":
+        if (args.length === 2) {
+          const oldPath = resolvePath(currentDir, args[0]);
+          const newPath = resolvePath(currentDir, args[1]);
+          compressFile(oldPath, newPath);
+        } else {
+          console.log("Operation failed");
+        }
+        break;
+      case "decompress":
+        if (args.length === 2) {
+          const oldPath = resolvePath(currentDir, args[0]);
+          const newPath = resolvePath(currentDir, args[1]);
+          decompressFile(oldPath, newPath);
+        } else {
+          console.log("Operation failed");
+        }
+        break;
+      case "os":
+        switch (args[0]) {
+          case "--EOL":
+            logEOL();
+            break;
+          case "--cpus":
+            logCPUs();
+            break;
+          case "--homedir":
+            logHomedir();
+            break;
+          case "--username":
+            logUsername();
+            break;
+          case "--architecture":
+            logArchitecture();
+            break;
+          default:
+            console.log(`Invalid input: ${args[0]}`);
         }
         break;
       default:
